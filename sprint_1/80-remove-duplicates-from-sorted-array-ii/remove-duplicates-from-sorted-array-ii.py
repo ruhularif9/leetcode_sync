d8ca034e38eecbd455517d862843e61b1eca5804
ruhularif9 <155552028+ruhularif9@users.noreply.github.com>
2025-11-01 09:04:14 +0530

    Time: 78 ms (86.71%) | Memory: 20.3 MB (79.42%) - LeetSync

--- sprint_1/80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ sprint_1/80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,4 +1,4 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+# __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0  
